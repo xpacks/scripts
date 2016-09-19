ba3881f90f3eb9323d8ab260aa34a6cb0e73ac11
Liviu Ionescu <ilg@livius.net>
2016-09-19 21:57:36 +0300

    bootstrap.sh: simplify setting of xpacks_repo_folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# -----------------------------------------------------------------------------
 # Bash script to load the scripts.git project.
+# -----------------------------------------------------------------------------
 
 # Prefer the environment location XPACKS_FOLDER, if defined,
 # but default to '.xpacks'.
-set +u
-if [[ -n "${XPACKS_FOLDER}" ]] && [[ -d "${XPACKS_FOLDER}" ]]
-then
-  xpacks_repo_folder="${XPACKS_FOLDER}"
-else
-  # As with other repositories, the default location for the xPacks
-  # repository is a hidden folder in the user home.
-  xpacks_repo_folder="$HOME/.xpacks"
-fi
-set -u
+xpacks_repo_folder="${XPACKS_REPO_FOLDER:-$HOME/.xpacks}"
+
+# -----------------------------------------------------------------------------
 
 # Update a single Git, or clone at first run.
 # $1 = absolute folder.
@@ -37,6 +32,8 @@ do_update_xpacks() {
   do_git_update "${xpacks_repo_folder}/ilg/$1.git" "https://github.com/xpacks/$1.git"
 }
 
+# -----------------------------------------------------------------------------
+
 if [ ! -d "${xpacks_repo_folder}" ]
 then
   echo "Creating ${xpacks_repo_folder}"
@@ -45,3 +42,5 @@ fi
 
 # Update third party xPacks
 do_update_xpacks "scripts"
+
+# -----------------------------------------------------------------------------
