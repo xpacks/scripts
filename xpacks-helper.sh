3dcb461c8ed5f3068773a9886954adc0258ab607
Liviu Ionescu <ilg@livius.net>
2017-03-04 16:41:38 +0200

    xpacks-helper: add explicit -path to variables
    - shorten echos

--- xpacks-helper.sh
+++ xpacks-helper.sh
@@ -7,30 +7,17 @@
 # -----------------------------------------------------------------------------
 
 # Get the full absolute path of the current script.
-script=$0
-if [[ "${script}" != /* ]]
+script_absolute_path=$0
+if [[ "${script_absolute_path}" != /* ]]
 then
   # Make relative path absolute.
-  script=$(pwd)/$0
+  script_absolute_path=$(pwd)/$0
 fi
 
 # -----------------------------------------------------------------------------
 
-# Include all 'xpacks-scripts.sh'
-source_tmp_file="$(mktemp)"
-
-# DEPRECATED (the source command is done inside do_install_xpack())
-_do_source_distributed_scripts() {
-  find "$xpacks_repo_folder" -maxdepth 4 -type f -name 'xpacks-helper.sh' -print \
-    | sed -e '/ilg\/scripts.git\/xpacks-helper.sh/d' \
-    | sed -n -e '/scripts\/xpacks-helper.sh/p' \
-    | sed -e 's/.*/source "&"/' > ${source_tmp_file}
-
-  source "${source_tmp_file}"
-  rm "${source_tmp_file}"
-}
-
 do_process_args() {
+
   writable=""
   verbose=""
   link=""
@@ -85,29 +72,28 @@ do_process_args() {
     esac
   done
 
-  local_xpacks_orig_folder="$(dirname $(dirname ${script}))/xpacks"
-  local_xpacks_folder="${local_xpacks_orig_folder}.tmp"
+  local_xpacks_orig_folder_path="$(dirname $(dirname ${script_absolute_path}))/xpacks"
+  local_xpacks_tmp_folder_path="${local_xpacks_orig_folder_path}.tmp"
 
-  if [ -d "${local_xpacks_folder}" ]
+  if [ -d "${local_xpacks_tmp_folder_path}" ]
   then
     echo
-    echo "Removing '${local_xpacks_folder}'..."
+    echo "Removing '${local_xpacks_tmp_folder_path}'..."
 
-    chmod -R +w "${local_xpacks_folder}"
-    rm -rf "${local_xpacks_folder}"
+    chmod -R +w "${local_xpacks_tmp_folder_path}"
+    rm -r -f "${local_xpacks_tmp_folder_path}"
   fi
 
-  mkdir -p ${verbose} "${local_xpacks_folder}"
+  mkdir -p ${verbose} "${local_xpacks_tmp_folder_path}"
   local file_name="NON_EDITABLE.txt"
-  mkdir -p ${verbose} "${local_xpacks_folder}"
-  echo "This folder was automatically generated." >"${local_xpacks_folder}/${file_name}"
-  echo "It contains links to all xPacks used within this project." >>"${local_xpacks_folder}/${file_name}"
-  echo "Except Git projects, all other should normally be read-only." >>"${local_xpacks_folder}/${file_name}"
-  echo "Prefereably do not use them directly, but prepare a nice 'generated' folder." >>"${local_xpacks_folder}/${file_name}"
+  mkdir -p ${verbose} "${local_xpacks_tmp_folder_path}"
+  echo "This folder was automatically generated." >"${local_xpacks_tmp_folder_path}/${file_name}"
+  echo "It contains links to all xPacks used within this project." >>"${local_xpacks_tmp_folder_path}/${file_name}"
+  echo "Except Git projects, all other should normally be read-only." >>"${local_xpacks_tmp_folder_path}/${file_name}"
+  echo "Prefereably do not use them directly, but prepare a nice 'generated' folder." >>"${local_xpacks_tmp_folder_path}/${file_name}"
 
-  generated_orig_folder="$(dirname $(dirname ${script}))/generated"
-  generated_folder="${generated_orig_folder}.tmp"
-
+  generated_orig_folder_path="$(dirname $(dirname ${script_absolute_path}))/generated"
+  generated_folder_path="${generated_orig_folder_path}.tmp"
 }
 
 # -----------------------------------------------------------------------------
@@ -117,6 +103,7 @@ do_process_args() {
 # $2 = author is (like "ilg")
 # $3 = project url
 do_install_xpack() {
+
   if [ $# -lt 3 ]
   then
     echo "do_install_xpack() requires 3 params"
@@ -125,126 +112,142 @@ do_install_xpack() {
 
   local url="$3"
 
-  local dst=${xpacks_repo_folder}/$2/$1.git
+  local dst_folder_path=${xpacks_repo_folder_path}/$2/$1.git
   echo "Checking '$1'..."
-  if [ ! -d "${dst}" ]
+  if [ ! -d "${dst_folder_path}" ]
   then
     # Quotes around "${branch}" are mandatory, otherwise will match always.
     if git ls-remote --heads --exit-code --quiet "${url}" "${branch}" > /dev/null 
     then
-      git clone --branch "${branch}" "${url}" "${dst}"
+      git clone --branch "${branch}" "${url}" "${dst_folder_path}"
     else
-      git clone "${url}" "${dst}"
+      git clone "${url}" "${dst_folder_path}"
     fi
-    (cd "${dst}"; git branch)
+    (cd "${dst_folder_path}"; git branch)
   fi
 
-  mkdir -p "${local_xpacks_folder}/$2"
+  mkdir -p "${local_xpacks_tmp_folder_path}/$2"
   if [ \( "${host_uname}" == "Darwin" \) -o \( "${host_uname}" == "Linux" \) ]
   then
-    ln -s ${verbose} "${dst}" "${local_xpacks_folder}/$2/$1"
+    echo "Symlinking '${dst_folder_path}' -> '$(basename $(basename ${local_xpacks_tmp_folder_path}))/$2/$1"
+    ln -s ${verbose} "${dst_folder_path}" "${local_xpacks_tmp_folder_path}/$2/$1"
   else
     # No symbolic links on Windows.
-    ln ${verbose} "${dst}" "${local_xpacks_folder}/$2/$1"
+    echo "Linking '${dst_folder_path}' -> '$(basename $(basename ${local_xpacks_tmp_folder_path}))/$2/$1'"
+    ln ${verbose} "${dst_folder_path}" "${local_xpacks_tmp_folder_path}/$2/$1"
   fi
 
   # If the cloned repo has a helper script, include its functions
   # to the current shell environment, to be used by the generate script.
-  if [ -f "${dst}/scripts/xpacks-helper.sh" ]
+  if [ -f "${dst_folder_path}/scripts/xpacks-helper.sh" ]
   then
-    source "${dst}/scripts/xpacks-helper.sh"
+    source "${dst_folder_path}/scripts/xpacks-helper.sh"
   fi
 }
 
 # -----------------------------------------------------------------------------
 
 do_greet() {
-  project_name="$(basename $(dirname $(dirname ${script})))"
+
+  project_name="$(basename $(dirname $(dirname ${script_absolute_path})))"
   echo
-  echo "* Generating xPacks for '${project_name}' *"
-  echo "Using xPacks from '${xpacks_repo_folder}'..."
+  echo "* Generate content for '${project_name}' *"
+  if [ -n "${symlink}" ]
+  then
+    echo "(by creating symbolic links to xPacks from '${xpacks_repo_folder_path}')"
+  elif [ -n "${link}" ]
+  then
+    echo "(by creating links to xPacks from '${xpacks_repo_folder_path}')"
+  else
+    echo "(by copying xPack files from '${xpacks_repo_folder_path}')"
+  fi
   echo
 }
 
 do_load_repo() {
-  if [ ! -d "${xpacks_repo_folder}" ]
-  then
-    update_script="$xpacks_repo_folder/ilg/scripts.git/update-xpacks-repo.sh"
 
-    bash "${update_script}"
+  if [ ! -d "${xpacks_repo_folder_path}" ]
+  then
+    update_script_path="${xpacks_repo_folder_path}/ilg/scripts.git/update-xpacks-repo.sh"
+
+    bash "${update_script_path}"
   fi
 }
 
 do_remove_dest() {
-  if [ -d "${generated_folder}" ]
+
+  if [ -d "${generated_folder_path}" ]
   then
     echo
-    echo "Removing '${generated_folder}'..."
+    echo "Removing '${generated_folder_path}'..."
 
-    chmod -R +w "${generated_folder}"
-    rm -rf "${generated_folder}"
+    chmod -R +w "${generated_folder_path}"
+    rm -rf "${generated_folder_path}"
   fi
 }
 
 do_create_dest() {
+
   local file_name="NON_EDITABLE.txt"
-  mkdir -p ${verbose} "${generated_folder}"
-  echo "This folder was automatically generated." >"${generated_folder}/${file_name}"
+  mkdir -p ${verbose} "${generated_folder_path}"
+  echo "This folder was automatically generated." >"${generated_folder_path}/${file_name}"
   if [ "$writable" != "y" ]
   then
-    echo "All files are set read-only and cannot be edited." >>"${generated_folder}/${file_name}"
+    echo "All files are set read-only and cannot be edited." >>"${generated_folder_path}/${file_name}"
   fi
 }
 
 do_protect() {
+
   if [ "$writable" == "y" ]
   then
     echo
     echo "Changing mode to R/W..."
-    chmod -R +w "${generated_folder}"/*
+    chmod -R +w "${generated_folder_path}"/*
   else
     echo
     echo "Changing mode to R/O..."
-    chmod -R -w "${generated_folder}"/*
+    chmod -R -w "${generated_folder_path}"/*
   fi
 }
 
 do_list() {
+
   echo
-  ls -l "${generated_folder}"
+  ls -l "${generated_folder_path}"
 }
 
 do_done() {
 
   # Remove original xpacks folder.
-  if [ -d "${local_xpacks_orig_folder}" ]
+  if [ -d "${local_xpacks_orig_folder_path}" ]
   then
-    if [ ! -n "$(find ${local_xpacks_orig_folder} -prune -empty)" ]
+    if [ ! -n "$(find ${local_xpacks_orig_folder_path} -prune -empty)" ]
     then
-      chmod -R +w "${local_xpacks_orig_folder}"/*
+      chmod -R +w "${local_xpacks_orig_folder_path}"/*
     fi
-    rm -r -f "${local_xpacks_orig_folder}"
+    rm -r -f "${local_xpacks_orig_folder_path}"
   fi
 
   # Rename the xpacks.net -> xpacks.
   echo
-  echo Renaming $(basename "${local_xpacks_folder}") "->" $(basename "${local_xpacks_orig_folder}")...
-  mv "${local_xpacks_folder}" "${local_xpacks_orig_folder}"
+  echo Renaming $(basename "${local_xpacks_tmp_folder_path}") "->" $(basename "${local_xpacks_orig_folder_path}")...
+  mv "${local_xpacks_tmp_folder_path}" "${local_xpacks_orig_folder_path}"
 
   # Remove original generated folder.
-  if [ -d "${generated_orig_folder}" ]
+  if [ -d "${generated_orig_folder_path}" ]
   then
-    if [ ! -n "$(find ${generated_orig_folder} -prune -empty)" ]
+    if [ ! -n "$(find ${generated_orig_folder_path} -prune -empty)" ]
     then
-      chmod -R +w "${generated_orig_folder}"/*
+      chmod -R +w "${generated_orig_folder_path}"/*
     fi
-    rm -r -f "${generated_orig_folder}"
+    rm -r -f "${generated_orig_folder_path}"
   fi
 
   # Rename the generated.tmp -> generated.
   echo
-  echo Renaming $(basename "${generated_folder}") "->" $(basename "${generated_orig_folder}")...
-  mv "${generated_folder}" "${generated_orig_folder}"
+  echo Renaming $(basename "${generated_folder_path}") "->" $(basename "${generated_orig_folder_path}")...
+  mv "${generated_folder_path}" "${generated_orig_folder_path}"
 
   echo
   echo "Done."
@@ -252,54 +255,59 @@ do_done() {
 
 # $1 = xpack name
 do_tell_xpack() {
+
   echo
   echo "Processing '$1'..."
 }
 
 # $1 = xpack name
 do_prepare_dest() {
-  dest_folder="${generated_folder}/$1"
 
-  echo "Creating '${dest_folder}'..."
+  dest_folder="${generated_folder_path}/$1"
+
+  echo "Creating '$1'..."
   mkdir -p ${verbose} "${dest_folder}"
 }
 
 # $1 = git path
 do_select_pack_folder() {
-  pack_folder="${xpacks_repo_folder}/$1"
+
+  pack_folder="${xpacks_repo_folder_path}/$1"
 }
 
 do_set_cube_folder() {
-  cube_folder="$(dirname $(dirname ${script}))/cube-mx"
+
+  cube_folder="$(dirname $(dirname ${script_absolute_path}))/cube-mx"
 }
 
 # $1 $2 ... source files or folders
 # $N destination folder
 do_add_content() {
+
   while [ $# -ge 1 ]
   do 
-    local dest="${dest_folder}/$(basename $1)"
+    local dst_folder_path="${dest_folder}/$(basename $1)"
 
     if [ -f "$1" ]
     then
       if [ "${link}" == "y" ]
       then
-        ln ${symlink} ${verbose} "$1" "${dest}"
+        ln ${symlink} ${verbose} "$1" "${dst_folder_path}"
       else
-        cp ${verbose} "$1" "${dest}"
+        cp ${verbose} "$1" "${dst_folder_path}"
       fi
     elif [ -d "$1" ]
     then
       cd "$1"
 
       # Create all intermediate folders 
-      find . -type d -exec mkdir -p ${verbose} "${dest}"/{} \;
+      find . -type d -exec mkdir -p ${verbose} "${dst_folder_path}"/{} \;
 
       if [ "${link}" == "y" ]
       then
-        find . -type f -exec ln ${symlink} ${verbose} "$1"/{} "${dest}"/{} \;
+        find . -type f -exec ln ${symlink} ${verbose} "$1"/{} "${dst_folder_path}"/{} \;
       else
-        find . -type f -exec cp ${verbose} "$1"/{} "${dest}"/{} \;
+        find . -type f -exec cp ${verbose} "$1"/{} "${dst_folder_path}"/{} \;
       fi
     else
       echo "Not supported content $1"
