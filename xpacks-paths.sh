5845b3fb40ce023f18db575ba8e921b56e68b9b7
Liviu Ionescu <ilg@livius.net>
2017-03-04 15:59:09 +0200

    xpacks-paths: suffix folder variables with _path

--- xpacks-paths.sh
+++ xpacks-paths.sh
@@ -6,27 +6,27 @@
 host_uname="$(uname)"
 if [ "${host_uname}" == "Darwin" ]
 then
-  xpacks_repo_folder="${XPACKS_REPO_FOLDER:-$HOME/Library/xPacks}"
-  xpacks_cache_folder="${XPACKS_CACHE_FOLDER:-$HOME/Library/Caches/xPacks}"
+  xpacks_repo_folder_path="${XPACKS_REPO_FOLDER:-$HOME/Library/xPacks}"
+  xpacks_cache_folder_path="${XPACKS_CACHE_FOLDER:-$HOME/Library/Caches/xPacks}"
 elif [ "${host_uname}" == "Linux" ]
 then
-  xpacks_repo_folder="${XPACKS_REPO_FOLDER:-$HOME/.xpacks}"
-  xpacks_cache_folder="${XPACKS_CACHE_FOLDER:-$HOME/.cache/xpacks}"
+  xpacks_repo_folder_path="${XPACKS_REPO_FOLDER:-$HOME/.xpacks}"
+  xpacks_cache_folder_path="${XPACKS_CACHE_FOLDER:-$HOME/.cache/xpacks}"
 elif [ "${host_uname:0:6}" == "MINGW64" ]
 then
-  xpacks_repo_folder="${XPACKS_REPO_FOLDER:-$HOME/AppData/Roaming/xPacks}"
-  xpacks_cache_folder="${XPACKS_CACHE_FOLDER:-$HOME/AppData/Local/Caches/xPacks}"
+  xpacks_repo_folder_path="${XPACKS_REPO_FOLDER:-$HOME/AppData/Roaming/xPacks}"
+  xpacks_cache_folder_path="${XPACKS_CACHE_FOLDER:-$HOME/AppData/Local/Caches/xPacks}"
 else
   echo "${host_uname} not supported"
   exit 1
 fi
 
-export xpacks_repo_folder
-export xpacks_cache_folder
+export xpacks_repo_folder_path
+export xpacks_cache_folder_path
 
 # -----------------------------------------------------------------------------
 
-xpacks_helper_path="${xpacks_repo_folder}/ilg/scripts.git/xpacks-helper.sh"
+xpacks_helper_path="${xpacks_repo_folder_path}/ilg/scripts.git/xpacks-helper.sh"
 
 # Check if the helper is present.
 if [ ! -f "${xpacks_helper_path}" ]
