c97c5c05340fa3d80dc3cd3594216b3c53082268
Liviu Ionescu <ilg@livius.net>
2017-03-04 15:47:27 +0200

    xpacks-paths: define and use xpacks_helper_path

--- xpacks-paths.sh
+++ xpacks-paths.sh
@@ -26,20 +26,20 @@ export xpacks_cache_folder
 
 # -----------------------------------------------------------------------------
 
+xpacks_helper_path="${xpacks_repo_folder}/ilg/scripts.git/xpacks-helper.sh"
+
 # Check if the helper is present.
-if [ ! -f "${xpacks_repo_folder}/ilg/scripts.git/xpacks-helper.sh" ]
+if [ ! -f "${xpacks_helper_path}" ]
 then
   mkdir -p "${HOME}/Downloads"
   echo "Downloading bootstrap.sh..."
   curl -L https://github.com/xpacks/scripts/raw/master/bootstrap.sh -o "${HOME}/Downloads/bootstrap.sh"
-  bash  "${HOME}/Downloads/bootstrap.sh"
+  bash "${HOME}/Downloads/bootstrap.sh"
 fi
 
 # -----------------------------------------------------------------------------
 
-helper_script="$xpacks_repo_folder/ilg/scripts.git/xpacks-helper.sh"
-
-# Include common definitions from helper script.
-source "${helper_script}"
+# Include common definitions from the helper script.
+source "${xpacks_helper_path}"
 
 
